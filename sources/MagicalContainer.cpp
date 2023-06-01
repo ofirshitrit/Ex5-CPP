1106899db5bc75a25cd40dcd63897fa21e321c1f
ofirshitrit <ofirha18@gmail.com>
2023-06-01 16:49:44 +0300

    working on the code according the tests

---
+++ sources/MagicalContainer.cpp
@@ -0,0 +1,175 @@
+//
+// Created by ofr on 5/28/23.
+//
+
+#include "MagicalContainer.hpp"
+
+using namespace std;
+using namespace ariel;
+
+void MagicalContainer::addElement(int element) {
+    elements.push_back(element);
+}
+
+size_t MagicalContainer::size() {
+    return elements.size();
+}
+
+void MagicalContainer::removeElement(int element) {
+    auto it = remove(elements.begin(), elements.end(), element);
+    if (it == elements.end()) throw runtime_error("Cant remove a non-existing element");
+    elements.erase(it, elements.end());
+}
+
+std::vector<int> &MagicalContainer::getElements(){
+    return this->elements;
+}
+
+
+bool MagicalContainer::AscendingIterator::operator==(const AscendingIterator& other) const {
+    return &container == &(other.container) && currentIndex == other.currentIndex;
+}
+
+bool MagicalContainer::AscendingIterator::operator!=(const AscendingIterator& other) const {
+    return !(*this == other);
+}
+
+
+MagicalContainer::AscendingIterator &
+MagicalContainer::AscendingIterator::operator=(const MagicalContainer::AscendingIterator &other) {
+    if (this != &other){
+       currentIndex = other.currentIndex;
+    }
+    return *this;
+}
+
+bool MagicalContainer::AscendingIterator::operator<(const MagicalContainer::AscendingIterator &other) const {
+    return currentIndex < other.currentIndex;
+}
+
+bool MagicalContainer::AscendingIterator::operator>(const MagicalContainer::AscendingIterator &other) const {
+
+    return currentIndex > other.currentIndex;
+}
+
+int MagicalContainer::AscendingIterator::operator*() const {
+    return 0;
+}
+
+MagicalContainer::AscendingIterator &MagicalContainer::AscendingIterator::operator++() {
+    ++currentIndex;
+    return *this;
+}
+
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::begin() {
+   //todo
+    return *this;
+}
+
+MagicalContainer::AscendingIterator MagicalContainer::AscendingIterator::end() {
+    //todo
+    return *this;
+}
+
+MagicalContainer::AscendingIterator::AscendingIterator(MagicalContainer &cont) : container(cont) , currentIndex(0) {
+    sort(cont.getElements().begin(),cont.getElements().end());
+}
+
+ MagicalContainer &MagicalContainer::AscendingIterator::getContainer()  {
+    return container;
+}
+
+
+MagicalContainer::SideCrossIterator::SideCrossIterator(MagicalContainer &cont) : container(cont){
+
+}
+
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::begin() {
+    return *this;
+}
+
+MagicalContainer::SideCrossIterator MagicalContainer::SideCrossIterator::end() {
+    return *this;
+}
+
+MagicalContainer::SideCrossIterator &
+MagicalContainer::SideCrossIterator::operator=(const MagicalContainer::SideCrossIterator &other) {
+    return *this;
+}
+
+int MagicalContainer::SideCrossIterator::operator*() const {
+    return 0;
+}
+
+MagicalContainer::SideCrossIterator &MagicalContainer::SideCrossIterator::operator++() {
+    return *this;
+}
+
+bool MagicalContainer::SideCrossIterator::operator==(const MagicalContainer::SideCrossIterator &other) const {
+    return false;
+}
+bool MagicalContainer::SideCrossIterator::operator!=(const MagicalContainer::SideCrossIterator &other) const {
+    return false;
+}
+
+bool MagicalContainer::SideCrossIterator::operator<(const MagicalContainer::SideCrossIterator &other) const {
+    return false;
+}
+
+bool MagicalContainer::SideCrossIterator::operator>(const MagicalContainer::SideCrossIterator &other) const {
+    return false;
+}
+
+MagicalContainer &MagicalContainer::SideCrossIterator::getContainer() const {
+    return container;
+}
+
+MagicalContainer::PrimeIterator::PrimeIterator(MagicalContainer &cont) : container(cont){
+
+}
+
+bool MagicalContainer::PrimeIterator::operator==(const PrimeIterator& other) const {
+    return false;
+}
+
+bool MagicalContainer::PrimeIterator::operator!=(const PrimeIterator& other) const {
+    return false;
+}
+
+
+MagicalContainer::PrimeIterator &
+MagicalContainer::PrimeIterator::operator=(const MagicalContainer::PrimeIterator &other) {
+
+    return *this;
+}
+
+bool MagicalContainer::PrimeIterator::operator<(const MagicalContainer::PrimeIterator &other) const {
+    return false;
+}
+
+bool MagicalContainer::PrimeIterator::operator>(const MagicalContainer::PrimeIterator &other) const {
+
+    return false;
+}
+
+int MagicalContainer::PrimeIterator::operator*() const {
+    return 0;
+}
+
+MagicalContainer::PrimeIterator &MagicalContainer::PrimeIterator::operator++() {
+    return *this;
+}
+
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::begin() {
+
+    return *this;
+}
+
+MagicalContainer::PrimeIterator MagicalContainer::PrimeIterator::end() {
+    PrimeIterator it(container);
+    return *this;
+}
+
+MagicalContainer &MagicalContainer::PrimeIterator::getContainer() const {
+    return container;
+}
---
+++ sources/MagicalContainer.hpp
@@ -0,0 +1,127 @@
+//
+// Created by ofr on 5/28/23.
+//
+
+#ifndef EX5_CPP_A_MAGICALCONTAINER_HPP
+#define EX5_CPP_A_MAGICALCONTAINER_HPP
+#include <iostream>
+#include <vector>
+#include <algorithm>
+namespace ariel {
+    class MagicalContainer {
+    private:
+        std::vector<int> elements;
+    public:
+        MagicalContainer() = default;
+        ~MagicalContainer() = default;
+
+        void addElement(int element);
+        size_t size();
+        void removeElement(int element);
+        std::vector<int>& getElements();
+
+        //for tidy
+        MagicalContainer(const MagicalContainer& other) = delete;
+        MagicalContainer& operator=(const MagicalContainer& other) = delete;
+        MagicalContainer(MagicalContainer&& other) = delete;
+        MagicalContainer& operator=(MagicalContainer&& other) = delete;
+
+        // Iterators
+        class AscendingIterator {
+        private:
+            MagicalContainer& container;
+            int currentIndex;
+
+        public:
+            AscendingIterator(MagicalContainer& cont);
+            AscendingIterator(AscendingIterator& other) : container(other.container) , currentIndex(other.currentIndex){}
+            ~AscendingIterator() = default;
+
+            AscendingIterator begin();
+            AscendingIterator end();
+
+            AscendingIterator& operator=(const AscendingIterator& other);
+            int operator*() const;
+            AscendingIterator& operator++();
+            MagicalContainer &getContainer();
+
+            bool operator==(const AscendingIterator& other) const;
+            bool operator!=(const AscendingIterator& other) const;
+            bool operator<(const AscendingIterator& other) const;
+            bool operator>(const AscendingIterator& other) const;
+
+            //for tidy
+            AscendingIterator(AscendingIterator&& other) = default;
+            AscendingIterator& operator=(AscendingIterator&& other) = delete;
+
+        };
+
+        class SideCrossIterator {
+        private:
+            MagicalContainer& container;
+
+        public:
+            SideCrossIterator(MagicalContainer& cont);
+            SideCrossIterator(SideCrossIterator& other) : container(other.container){}
+            ~SideCrossIterator() = default;
+
+            SideCrossIterator begin();
+            SideCrossIterator end();
+
+            SideCrossIterator& operator=(const SideCrossIterator& other);
+            int operator*() const;
+            SideCrossIterator& operator++();
+            MagicalContainer &getContainer() const;
+
+            bool operator==(const SideCrossIterator& other) const;
+            bool operator!=(const SideCrossIterator& other) const;
+            bool operator<(const SideCrossIterator& other) const;
+            bool operator>(const SideCrossIterator& other) const;
+
+            //for tidy
+            SideCrossIterator(SideCrossIterator&& other) = delete;
+            SideCrossIterator& operator=(SideCrossIterator&& other) = delete;
+
+        };
+
+        class PrimeIterator {
+        private:
+            MagicalContainer& container;
+
+        public:
+            PrimeIterator(MagicalContainer& cont);
+            PrimeIterator(PrimeIterator& other) : container(other.container){}
+            ~PrimeIterator() = default;
+
+            PrimeIterator begin();
+            PrimeIterator end();
+
+            PrimeIterator& operator=(const PrimeIterator& other);
+            int operator*() const;
+            PrimeIterator& operator++();
+
+            MagicalContainer &getContainer() const;
+
+            bool operator==(const PrimeIterator& other) const;
+            bool operator!=(const PrimeIterator& other) const;
+            bool operator<(const PrimeIterator& other) const;
+            bool operator>(const PrimeIterator& other) const;
+
+            //for tidy
+            PrimeIterator(PrimeIterator&& other) = delete;
+            PrimeIterator& operator=(PrimeIterator&& other) = delete;
+        };
+
+
+    };
+
+};
+
+
+
+
+
+
+
+
+#endif //EX5_CPP_A_MAGICALCONTAINER_HPP
